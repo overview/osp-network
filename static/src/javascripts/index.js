7733a66b363e57080b29bfce1ba8455a18d97b03
David McClure <davidwilliammcclure@gmail.com>
2015-04-30 20:14:09 -0700

    Resetting the .js app.

--- static/src/javascripts/index.js
+++ static/src/javascripts/index.js
@@ -1,16 +1,3 @@
 
 
-var React = require('react');
-var Fluxxor = require('fluxxor');
-var Network = require('./components/network');
-var stores = require('./stores');
-var actions = require('./actions');
-
-
-var flux = new Fluxxor.Flux(stores, actions);
-
-
-React.render(
-  <Network flux={flux} />,
-  document.getElementById('network')
-);
+console.log('test');
