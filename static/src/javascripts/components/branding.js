d1f137aa54d8962bff23b75b9e05eb9f5cf7eac3
David McClure <davidwilliammcclure@gmail.com>
2015-05-08 12:18:50 -0700

    Drops the count-up times back down to 2 seconds.

--- static/src/javascripts/components/branding.js
+++ static/src/javascripts/components/branding.js
@@ -51,9 +51,9 @@ module.exports = React.createClass({
    * Fancy counting animations! Because I can!
    */
   componentDidMount: function() {
-    new countUp('s-count', 0, 1177109, 0, 3).start();
-    new countUp('t-count', 0, 744036,  0, 3).start();
-    new countUp('c-count', 0, 9858427, 0, 3).start();
+    new countUp('s-count', 0, 1177109, 0, 2).start();
+    new countUp('t-count', 0, 744036,  0, 2).start();
+    new countUp('c-count', 0, 9858427, 0, 2).start();
   }
 
 
